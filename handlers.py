da962bb7b344724d8e3d45acabb40620eed26582
kitchen-bear <kitchen.bear75@gmail.com>
2025-01-30 13:54:58 +1000

    update wallet for alfa-send functions to RyoL wallet:7

--- handlers.py
+++ handlers.py
@@ -425,7 +425,7 @@ async def msend_trans(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int
         transfer_submit = await submit_transaction_rpc(user_id, user_pass)
         if not transfer_submit:
             await update.message.reply_text("Can't submit transaction. Check your balance. Try again later. Or call support.")
-            return ConversationHandler.
+            return ConversationHandler.END
         if transfer_submit:
              tx_fin_value = context.user_data.get['tx_hash_final']
              await update.message.reply_text(f"Transaction submitted successfully! Tx Hash: {tx_fin_value}")
